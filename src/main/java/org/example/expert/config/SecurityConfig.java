bac7e4a75f9bcea1cb85f6244f117d11951cb2b1
KimKiHong-1111 <rlghd0157@gmail.com>
2025-03-11 20:15:50 +0900

    feat:Lv2 8.security 기능구현

---
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+    private final JwtSecurityFilter jwtSecurityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtSecurityFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
